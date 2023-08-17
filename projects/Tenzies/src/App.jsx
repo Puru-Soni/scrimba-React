d3a09d393471663967beb83e2c41765078a460d3
Puru <purusoni05@gmail.com>
2023-08-17 15:21:28 +0530

    added new project - tenzies

--- projects/Tenzies/src/App.jsx
+++ projects/Tenzies/src/App.jsx
@@ -1,7 +1,78 @@
 import React from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+import Die from "./Die";
 
-const App = () => {
-	return <div>App</div>;
-};
+export default function App() {
+	const [dice, setDice] = React.useState(allNewDice()); // array of objects
+	const [tenzies, setTenzies] = React.useState(false); // Win or lose
 
-export default App;
+	React.useEffect(() => {
+		const firstValue = dice[0].value;
+		const allHeld = dice.every((die) => die.held);
+		const allSameNumber = dice.every((die) => die.value === firstValue);
+		if (allHeld && allSameNumber) {
+			setTenzies(true);
+		}
+	}, [dice]);
+
+	function randomDieValue() {
+		return Math.ceil(Math.random() * 6);
+	}
+
+	function allNewDice() {
+		const newArray = [];
+		for (let i = 0; i < 10; i++) {
+			const newDie = {
+				value: randomDieValue(),
+				held: false,
+				id: i + 1,
+			};
+			newArray.push(newDie);
+		}
+		return newArray;
+	}
+
+	function rollUnheldDice() {
+		if (!tenzies) {
+			setDice((oldDice) =>
+				oldDice.map((die, i) =>
+					die.held ? die : { value: randomDieValue(), held: false, id: i + 1 },
+				),
+			);
+		} else {
+			// reset
+			setDice(allNewDice());
+			setTenzies(false);
+		}
+	}
+
+	function holdDice(id) {
+		setDice((prevDice) =>
+			prevDice.map((die) => {
+				return die.id === id ? { ...die, held: !die.held } : die;
+			}),
+		);
+	}
+
+	const diceElements = dice.map((die) => (
+		<Die key={die.id} {...die} hold={() => holdDice(die.id)} />
+	));
+
+	const { width, height } = useWindowSize();
+
+	return (
+		<main>
+			{tenzies && <Confetti width={width} height={height} />}
+			<h1>Tenzies</h1>
+			<p>
+				Roll until all dice are the same. Click each die to freeze it at its
+				current value between rolls.
+			</p>
+			<div className="die-container">{diceElements}</div>
+			<button className="roll-dice" onClick={rollUnheldDice}>
+				{tenzies ? "Reset Game" : "Roll"}
+			</button>
+		</main>
+	);
+}
